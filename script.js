758587a5ebba118049c1f24a722f70ba5c279692
FlorianWeyrauch <weyrauch-florian@web.de>
2025-05-03 14:51:13 +0200

    type color bg css class

--- script.js
+++ script.js
@@ -8,29 +8,6 @@ async function init() {
     await loadPokemon();
 }
 
-const typeColors = {
-    normal: '#A8A77A',
-    fire: '#EE8130',
-    water: '#6390F0',
-    electric: '#F7D02C',
-    grass: '#7AC74C',
-    ice: '#96D9D6',
-    fighting: '#C22E28',
-    poison: '#A33EA1',
-    ground: '#E2BF65',
-    flying: '#A98FF3',
-    psychic: '#F95587',
-    bug: '#A6B91A',
-    rock: '#B6A136',
-    ghost: '#735797',
-    dragon: '#6F35FC',
-    dark: '#705746',
-    steel: '#B7B7CE',
-    fairy: '#D685AD'
-};
-
-
-
 //holt sich die gesamten pokemonUrls aus der Api und speichert diese in einer globalen Liste ab
 async function getPokemonUrl() {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`);
@@ -75,11 +52,8 @@ function showPokemon(pokemonData) {
     for (let i = 0; i < pokemonData.length; i++) {
         const pokemon = pokemonData[i];
         const mainType = pokemon.types[0].type.name;
-        const bgColor = typeColors[mainType];
-
         const card = document.createElement('div');
-        card.className = 'pokemon-card';
-        card.style.backgroundColor = bgColor;
+        card.className = `pokemon-card ${mainType}`; // z. B. "pokemon-card grass"
         card.innerHTML = templatePokemonCard(pokemon);
         listContainer.appendChild(card);
 
@@ -91,7 +65,7 @@ function showPokemon(pokemonData) {
 function showPokemonType(pokemon) {
     let typeContent = document.getElementById("card_type_" + pokemon.id);
     pokemon.types.forEach(element => {
-        typeContent.innerHTML += `<span class="pokemon-type">${capitalizeFirstLetter(element.type.name)}</span> `;
+        typeContent.innerHTML += `<span class="pokemon-type ${element.type.name}">${capitalizeFirstLetter(element.type.name)}</span> `;
     });
 }
 
